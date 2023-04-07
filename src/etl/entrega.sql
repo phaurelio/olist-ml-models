63472529d4d3f8b881a0a4f25ae227f668cb5a7f
phaurelio@hotmail.com <phaurelio@hotmail.com>
2023-04-07 23:01:18 +0000

    Fixing entrega

--- src/etl/entrega.sql
+++ src/etl/entrega.sql
@@ -17,6 +17,7 @@ ON t1.idPedido = t2.idPedido
 
 WHERE dtPedido < '2018-01-01'
 AND dtPedido >= add_months('2018-01-01', -6)
+AND idVendedor IS NOT NULL
 
 GROUP BY t1.idPedido,
          t2.idVendedor,
@@ -28,6 +29,7 @@ GROUP BY t1.idPedido,
 )
 
 SELECT 
+    '2018-01-01' AS dtReference,
     idVendedor,
     COUNT(DISTINCT CASE WHEN date(coalesce(dtEntregue, '2018-01-01')) > date(dtEstimativaEntrega) THEN idPedido END) / COUNT(DISTINCT CASE WHEN descSituacao = 'delivered' THEN idPedido END) AS pctPedidoAtraso,
     count(distinct case when descSituacao = 'canceled' then idPedido end) / count(distinct idPedido) AS pctPedidoCancelado,
